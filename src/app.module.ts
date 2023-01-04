da522cd388daeab07af4a139be921cdce594d529
Dmytro Voronov <dimolg22@gmail.com>
2023-01-04 13:21:04 +0200

    add configeModule2

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import * as process from "process";
     SequelizeModule.forRoot({
     dialect: 'postgres',
     host: process.env.POSTGRES_HOST,
-    port: +process.env.POSTGRES_PORT,
+    port: Number(process.env.POSTGRES_PORT),
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
